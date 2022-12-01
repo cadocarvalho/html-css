5a5cb15078c4178d65448f6c022936b40a173bc6
cadocarvalho <ricardocarvalhoo48@hotmail.com>
2022-12-01 18:14:16 -0300

    att

--- exercicios/ex003/func.js
+++ exercicios/ex003/func.js
@@ -1,7 +1,12 @@
-document.getElementById('myvideo').addEventListener('ended',videoEndHandler,false);
-function videoEndHandler(e) {
-    document.getElementById('btn').style.display = "block";
-}
+var aud = document.getElementsByTagName('iframe')[0];
+var approxTime = 0
+aud.ontimeupdate = function(){
+    var currentTime = Math.floor(aud.currentTime);
+    if (currentTime !== approxTime) {
+        approxTime = currentTime;
+        if (approxTime === 2) {
+            document.querySelector('.btn').classList.add("show-btn")
+        }
+    }
+};
 
-
- 
